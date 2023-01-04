aae5dc51dce49c6bbe23bc57dfed7588e5e2ff32
Michael Mederos <mikemederos01@gmail.com>
2023-01-03 23:10:49 -0500

    Fixed intellisense

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 using namespace cv;
 int main(int argc, char** argv )
 {
-
     Mat image;
     image = imread("C:/Users/mikef/OneDrive/Pictures/Camera Roll/WIN_20230103_20_03_40_Pro.jpg");
     if ( !image.data )
