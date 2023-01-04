aee13a9531e624b213081aa1a2958d4f6e15939b
Michael Mederos <mikemederos01@gmail.com>
2023-01-03 23:24:54 -0500

    video capture

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 int main(int argc, char** argv )
 {
     Mat image;
-    image = imread("C:/Users/mikef/OneDrive/Pictures/Camera Roll/WIN_20230103_20_03_40_Pro.jpg");
-    if ( !image.data )
-    {
-        printf("No image data /n");
-        return -1;
+    namedWindow("Display window");
+    VideoCapture cap(0);
+
+    if(!cap.isOpened()){
+        cout << "Cannot open camera";
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+
+    while (true)
+    {
+        cap >> image;
+        imshow("Display window", image);
+        waitKey(25);
+    }
     return 0;
 }
