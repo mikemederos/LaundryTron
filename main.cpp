29673573af28ece0c87ba06ac0ab24bae47af313
Michael Mederos <mikemederos01@gmail.com>
2023-01-03 20:42:03 -0500

    hi

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
+using namespace cv;
+int main(int argc, char** argv )
+{
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    Mat image;
+    image = imread("C:/Users/mikef/OneDrive/Pictures/Camera Roll/WIN_20230103_20_03_40_Pro.jpg");
+    if ( !image.data )
+    {
+        printf("No image data /n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
 }
